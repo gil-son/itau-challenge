bd2b05ec1748774f40ca5730fd2f45022e7a4acb
gil-son <gilszon@gmail.com>
2024-05-21 20:40:04 -0300

    Entidade para a Transferencia

--- app/src/main/java/com/transferencia/entities/Transferencia.java
+++ app/src/main/java/com/transferencia/entities/Transferencia.java
@@ -6,16 +6,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @ToString
 @NoArgsConstructor
 @Entity
-@Table(name = "tb_club")
-public class TransferenciaRequest {
+@Table(name = "tb_transferencia")
+public class Transferencia implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
     private String id_transferencia;
     private String idCliente;
     private Double valor;
-    private ContaTransacao conta;
+    private String idOrigem;
+    private String idDestino;
 }
