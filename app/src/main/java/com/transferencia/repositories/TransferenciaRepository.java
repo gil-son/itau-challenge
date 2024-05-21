92488c2c707c750988a7e78afe577c16a457b9dd
gil-son <gilszon@gmail.com>
2024-05-21 20:42:41 -0300

    Armazenar os dados de Transferencia

--- app/src/main/java/com/transferencia/repositories/TransferenciaRepository.java
+++ app/src/main/java/com/transferencia/repositories/TransferenciaRepository.java
@@ -1,4 +1,7 @@
 package com.transferencia.repositories;
 
-public class TransferenciaRepository {
+import com.transferencia.entities.Transferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferenciaRepository extends JpaRepository<Transferencia, String> {
 }
