dce5ad7994e212fdd1b42e7d141bde347ebf2412
gil-son <gilszon@gmail.com>
2024-05-21 20:38:00 -0300

    Controller para a rota de transferencia

--- app/src/main/java/com/transferencia/controllers/TransferenciaController.java
+++ app/src/main/java/com/transferencia/controllers/TransferenciaController.java
@@ -1,26 +1,52 @@
 package com.transferencia.controllers;
 
-import com.transferencia.dto.ClienteDTO;
+import com.transferencia.dto.TransferenciaResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.transferencia.dto.TransferenciaRequestDTO;
+import com.transferencia.services.NotificacaoService;
 import com.transferencia.services.TransferenciaService;
+import com.transferencia.services.ValidacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/transferencia")
-public class TransferenciaClienteController {
+public class TransferenciaController {
 
-    private final TransferenciaService transferenciaService;
+    private static final Logger logger = LoggerFactory.getLogger(TransferenciaController.class);
+    @Autowired
+    private ValidacaoService validacaoService;
 
     @Autowired
-    public TransferenciaClienteController(TransferenciaService transferenciaService) {
-        this.transferenciaService = transferenciaService;
-    }
+    private TransferenciaService transferenciaService;
 
-    @GetMapping
-    public ClienteDTO getData() {
-        return transferenciaService.getDataFromApi();
-    }
+    @Autowired
+    private NotificacaoService notificacaoService;
 
+    @PostMapping
+    public ResponseEntity<?> realizarTransferencia(@RequestBody TransferenciaRequestDTO transferenciaRequestDTO) {
+
+        logger.info("TransferenciaController - realizarTransferencia - DTO: {}", transferenciaRequestDTO);
+        // Validar a transferência
+        String validacaoErro = validacaoService.validarTransferencia(transferenciaRequestDTO);
+        if (validacaoErro != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(validacaoErro);
+        }
+
+        // Realizar a transferência
+        String idTransferencia = transferenciaService.realizarTransferencia(transferenciaRequestDTO);
+
+        // Notificar o BACEN
+        boolean sucesso = notificacaoService.notificarBacen(transferenciaRequestDTO);
+        if (!sucesso) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Falha ao notificar o BACEN - Esse dado será armazenado em fila");
+        }
+
+        // Criar o objeto de resposta
+        TransferenciaResponseDTO responseDTO = new TransferenciaResponseDTO(idTransferencia);
+
+        return ResponseEntity.ok(responseDTO);
+    }
 }
