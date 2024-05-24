6de65e62490fd9eb6fae23e8f5d5ded98d8ffdce
gil-son <gilszon@gmail.com>
2024-05-23 22:01:50 -0300

    mensagem em formato json

--- app/src/main/java/com/transferencia/dto/TransferenciaRequestDTO.java
+++ app/src/main/java/com/transferencia/dto/TransferenciaRequestDTO.java
@@ -4,14 +4,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.json.JSONObject;
 
 @Setter
 @Getter
-@ToString
 @NoArgsConstructor
 public class TransferenciaRequestDTO {
 
     private String idCliente;
     private Double valor;
     private ContaTransacaoDTO conta;
+
+    @Override
+    public String toString(){
+
+        JSONObject jsonInterno = new JSONObject();
+        jsonInterno.put("idOrigem", this.conta.getIdOrigem());
+        jsonInterno.put("idDestino", this.conta.getIdDestino());
+
+        JSONObject jsonExterno = new JSONObject();
+        jsonExterno.put("idCliente", this.idCliente);
+        jsonExterno.put("valor", this.valor);
+        jsonExterno.put("conta", jsonInterno);
+
+        return jsonExterno.toString();
+    }
+
+
 }
